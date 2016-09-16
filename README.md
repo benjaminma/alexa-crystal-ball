f04874514608792cfeb94f6fb2e7fa71eb5de3fe
Ben Ma <git.app@benjaminma.com>
2016-09-15 22:35:13 -0700

    Modify alexa/skill-sample-nodejs-fact template

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # alexa-crystal-ball
-An Amazon Alexa Skill to predict life's important yes/no questions
+An Amazon Alexa Skill to predict life's most important yes/no questions
+
+## License
+Apache v2.0 <https://www.apache.org/licenses/LICENSE-2.0>
---
+++ src/index.js
@@ -0,0 +1,119 @@
+'use strict';
+var Alexa = require('alexa-sdk');
+
+var APP_ID = undefined; //OPTIONAL: replace with "amzn1.echo-sdk-ams.app.[your-unique-value-here]";
+var SKILL_NAME = 'Crystal Ball';
+
+/**
+ * Array containing preditions.
+ */
+var PREDICTIONS = {
+    YES: [
+        'As I see it... yes.',
+        'Most likely.',
+        'My sources say... yes.',
+        'The outlook is... good.',
+        'The signs point to... yes.',
+        'Very likely.',
+        'Yes, definitely.',
+        'Yes, in due time.',
+        'Yes.',
+        'You can count on it.'
+    ],
+    NO: [
+        'As I see it... no.',
+        'Definitely... not.',
+        'Don\'t bet on it.',
+        'Don\'t count on it.',
+        'I have my doubts.',
+        'My sources say... no.',
+        'Not a chance.',
+        'The outlook is... not so good.',
+        'The signs point to... no.'
+    ],
+    MAYBE: [
+        'Concentrate and ask again.',
+        'Perhaps... in your dreams!',
+        'The outlook is... so so.',
+        'Who knows?'
+    ]
+};
+
+exports.handler = function(event, context) {
+    var alexa = Alexa.handler(event, context);
+    alexa.APP_ID = APP_ID;
+    alexa.registerHandlers(handlers);
+    alexa.execute();
+};
+
+var handlers = {
+    'LaunchRequest': function () {
+        this.emit('GetFact');
+    },
+    'GetNewFactIntent': function () {
+        this.emit('GetFact');
+    },
+    'GetFact': function () {
+		// Get a random prediction
+        var prediction = getRandomPrediction();
+        var speechOutput = (rand(1, 100) >= 80) ? 'Hmm... ' : '';
+        speechOutput += prediction.output;
+        this.emit(':tellWithCard', speechOutput, SKILL_NAME, prediction.type);
+    },
+    'AMAZON.HelpIntent': function () {
+        var speechOutput = 'I am a crystal ball. Ask me a yes or no question, or, you can say exit... What would you like to ask?';
+        var reprompt = 'What would you like to ask?';
+        this.emit(':ask', speechOutput, reprompt);
+    },
+    'AMAZON.CancelIntent': function () {
+        this.emit(':tell', 'Goodbye!');
+    },
+    'AMAZON.StopIntent': function () {
+        this.emit(':tell', 'Goodbye!');
+    }
+};
+
+/**
+ * Return a random integer between min and max (inclusive)
+ * @param {number} min
+ * @param {number} max
+ * @return {number}
+ */
+var rand = function(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+/**
+ * Return a random item from array (or undefined if array is invalid)
+ * @param {Array} arr
+ * @return {*}
+ */
+var randFromArray = function(arr) {
+    if (arr instanceof Array && arr.length > 0) {
+        var l = arr.length;
+        var idx = rand(0, l - 1);
+        return arr[idx];
+    } else {
+        return undefined;
+    }
+};
+
+/**
+ * Return a random prediction object
+ * @return {{output: string, type: string}}
+ */
+var getRandomPrediction = function() {
+    var prediction = {};
+    var chance = rand(1, 100);
+    if (chance >= 60) {
+        prediction.type = 'Yes';
+        prediction.output = randFromArray(PREDICTIONS.YES);
+    } else if (chance >= 20) {
+        prediction.type = 'No';
+        prediction.output = randFromArray(PREDICTIONS.NO);
+    } else {
+        prediction.type = 'Maybe';
+        prediction.output = randFromArray(PREDICTIONS.MAYBE);
+    }
+    return prediction;
+};
